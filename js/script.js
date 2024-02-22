c4b53d45bd9ba5a40a3689b997ba8e6873dfcc5d
Bruno César <brunocesar.tenorio1@gmail.com>
2024-02-22 18:18:36 -0300

    updating js

--- js/script.js
+++ js/script.js
@@ -1,54 +1,56 @@
-class Countdown {
-  constructor(futureDate) {
-    this.futureDate = futureDate;
-  }
-  get _futureDate() {
-    return new Date(this.futureDate);
-  }
-  get _actualDate() {
-    return new Date();
-  }
-  get _totalSeconds() {
-    return this._futureDate.getTime() - this._actualDate.getTime();
-  }
-  get days() {
-    return Math.floor(this._totalSeconds / (24 * 60 * 60 * 1000));
-  }
-  get hours() {
-    return Math.floor(this._totalSeconds / (60 * 60 * 1000));
-  }
-  get minutes() {
-    return Math.floor(this._totalSeconds / (60 * 1000));
-  }
-  get seconds() {
-    return Math.floor(this._totalSeconds / 1000);
-  }
-  get total() {
-    const days = this.days;
-    const hours = this.hours % 24;
-    const minutes = this.minutes % 60;
-    const seconds = this.seconds % 60;
-    return {
-      days,
-      hours,
-      minutes,
-      seconds,
-    };
-  }
-}
-const newYearTime = new Countdown("01 January 2025 00:00:00 GMT-0300");
-
 const days = document.querySelector("[data-days]");
 const hours = document.querySelector("[data-hours]");
 const minutes = document.querySelector("[data-minutes]");
 const seconds = document.querySelector("[data-seconds]");
+if (days && hours && minutes && seconds) {
+  class Countdown {
+    constructor(newYearDate) {
+      this.newYearDate = newYearDate;
+    }
+    get _actualDate() {
+      return new Date();
+    }
+    get _newYearDate() {
+      return new Date(this.newYearDate);
+    }
+    get _totalSeconds() {
+      return (this._newYearDate - this._actualDate) / 1000;
+    }
+    get _days() {
+      return Math.floor(this._totalSeconds / 24 / 60 / 60);
+    }
+    get _hours() {
+      return Math.floor(this._totalSeconds / 60 / 60);
+    }
+    get _minutes() {
+      return Math.floor(this._totalSeconds / 60);
+    }
+    get _seconds() {
+      return Math.floor(this._totalSeconds);
+    }
+    get _total() {
+      const days = this._days;
+      const hours = this._hours % 24;
+      const minutes = this._minutes % 60;
+      const seconds = this._seconds % 60;
+      return {
+        days,
+        hours,
+        minutes,
+        seconds,
+      };
+    }
+  }
+  const countdown = new Countdown("01 January 2025 00:00:00 GMT-0300");
 
-setInterval(() => {
-  days.innerText = formatDate(newYearTime.total.days);
-  hours.innerText = formatDate(newYearTime.total.hours);
-  minutes.innerText = formatDate(newYearTime.total.minutes);
-  seconds.innerText = formatDate(newYearTime.total.seconds);
-}, 1000);
-function formatDate(time) {
-  return time < 10 ? `0${time}` : `${time}`;
+  function formatTime(time) {
+    return time < 10 ? `0${time}` : `${time}`;
+  }
+
+  setInterval(() => {
+    days.innerText = formatTime(countdown._total.days);
+    hours.innerText = formatTime(countdown._total.hours);
+    minutes.innerText = formatTime(countdown._total.minutes);
+    seconds.innerText = formatTime(countdown._total.seconds);
+  }, 1000);
 }
